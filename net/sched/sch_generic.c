a9c349c4dc6bd1ec78063af7eb3aeae8348665b5
Luka Bradesko <luka.bradesko@gmail.com>
2019-06-30 11:58:03 +0200

    Add MACH Modem timeout kernel message

--- net/sched/sch_generic.c
+++ net/sched/sch_generic.c
@@ -457,6 +457,7 @@ static void dev_watchdog(struct timer_list *t)
 			}
 
 			if (some_queue_timedout) {
+				printk(KERN_INFO "MACH: Modem transmit queue timed out\n");
 				WARN_ONCE(1, KERN_INFO "NETDEV WATCHDOG: %s (%s): transmit queue %u timed out\n",
 				       dev->name, netdev_drivername(dev), i);
 				dev->netdev_ops->ndo_tx_timeout(dev);
